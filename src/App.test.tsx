3b17723e0e3c80a019acab0341a323c6196ea560
Marcus Ong <marcus.ong.qy@gmail.com>
2022-05-16 15:18:49 +0800

    added basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import {
+  getByText,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(
+const AppWithStore = () => {
+  return (
     <Provider store={store}>
       <App />
     </Provider>
   );
+};
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+test("loading screen appears", () => {
+  render(<AppWithStore />);
+  expect(screen.queryByText(/loading/i)).toBeInTheDocument();
+});
+
+test("login button has corect text", async () => {
+  render(<AppWithStore />);
+  const loginButton = screen.getByRole("button", { name: "Login" });
+
+  await waitFor(() => {
+    expect(loginButton).toBeInTheDocument();
+  });
 });
