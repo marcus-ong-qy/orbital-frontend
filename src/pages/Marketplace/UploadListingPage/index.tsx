7c049c775dcb5e46669d45ee32b924b577b97619
Marcus Ong <marcus.ong.qy@gmail.com>
2022-06-13 22:27:30 +0800

    created upload listing form

--- src/pages/Marketplace/UploadListingPage/index.tsx
+++ src/pages/Marketplace/UploadListingPage/index.tsx
@@ -1,17 +1,115 @@
 import { useAppDispatch, useAppSelector } from '../../../app/hooks'
+import {
+  EntryDiv,
+  PostForm,
+  ItemPicture,
+  LeftDiv,
+  PictureButton,
+  PostButton,
+  RightDiv,
+  StyledUploadListingPage,
+  UploadListingDiv,
+  TitleHighlight,
+  TitleDiv,
+  EntryName,
+  EntryArea,
+  TypeSelection,
+} from './styles/UploadListingPage.styled'
+
+import defaultPic from '../../../assets/picture.png'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+import { theme } from '../../../styles/Theme'
+import InputField from '../../../components/common/InputFields/InputField'
 
 const UploadListingPage = () => {
   const dispatch = useAppDispatch()
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm({ mode: 'onChange' })
+
   const { newListing } = useAppSelector((state) => state.marketplace_reducer)
   const { name, price, description, typeOfTransaction, deliveryInformation, tags, imageURL } = {
     ...newListing,
   }
+  const { navTitleFont, p } = { ...theme.typography.fontSize }
+
+  const [listingType, setListingType] = useState<'Rent' | 'Sell'>(typeOfTransaction)
+
+  const uploadPicture = () => {}
+
+  const onSubmit = () => {}
 
   return (
-    <div>
-      <h1>Upload Listing Page</h1>
-      <div>Type: {typeOfTransaction}</div>
-    </div>
+    <StyledUploadListingPage>
+      <TitleDiv fontType={navTitleFont}>
+        What are you <TitleHighlight type={listingType}>{`${listingType}ing `}</TitleHighlight>
+        today?
+      </TitleDiv>
+      <UploadListingDiv>
+        <LeftDiv>
+          <ItemPicture src={defaultPic} />
+          <PictureButton text="Upload Picture" onClick={uploadPicture} />
+        </LeftDiv>
+        <RightDiv>
+          <PostForm onSubmit={handleSubmit(onSubmit)} noValidate>
+            <EntryDiv type="input">
+              <EntryName fontType={p}> Listing Type&nbsp;</EntryName>
+              <TypeSelection
+                placeholder="Listing Type"
+                value={listingType}
+                {...register('listing-type', { required: true })}
+                onChange={(e) => {
+                  setValue('listing-type', e.target.value)
+                  setListingType(e.target.value as 'Rent' | 'Sell')
+                }}
+              >
+                <option value="Rent">Rent</option>
+                <option value="Sell">Sell</option>
+              </TypeSelection>
+            </EntryDiv>
+            <EntryDiv type="input">
+              <EntryName fontType={p}>Item Name&nbsp;</EntryName>
+              <InputField title="item-name" placeholder="Item Name" register={register} />
+            </EntryDiv>
+            <EntryDiv type="input">
+              <EntryName fontType={p}>
+                Price <b>SG$</b> {listingType === 'Rent' && '/day'}
+              </EntryName>
+              <InputField title="price" placeholder="Price" register={register} />
+            </EntryDiv>
+            <EntryDiv type="textarea">
+              <EntryName fontType={p}>Product Description&nbsp;</EntryName>
+              <EntryArea
+                placeholder="Description of your Product"
+                {...register('product-description', { required: true })}
+              />
+            </EntryDiv>
+            <EntryDiv type="textarea">
+              <EntryName fontType={p}>Deal Information&nbsp;</EntryName>
+              <EntryArea
+                placeholder="Include details such as Delivery and Payment methods"
+                {...register('deal-information', { required: true })}
+              />
+            </EntryDiv>
+            <EntryDiv type="input">
+              <EntryName fontType={p}>Tags </EntryName>
+              <InputField
+                title="tags"
+                placeholder="Seperate keywords with commas (,)"
+                register={register}
+              />
+            </EntryDiv>
+            <PostButton type="submit" text="Post" />
+          </PostForm>
+        </RightDiv>
+      </UploadListingDiv>
+    </StyledUploadListingPage>
   )
 }
 
--- src/pages/Marketplace/UploadListingPage/styles/UploadListingPage.styled.ts
+++ src/pages/Marketplace/UploadListingPage/styles/UploadListingPage.styled.ts
@@ -1,6 +1,113 @@
-import styled from 'styled-components'
-import { styledPageCss } from '../../../../styles/index.styled'
+import styled, { css } from 'styled-components'
+import Button from '../../../../components/common/Button/Button'
+import { borderedGreyDivCss, fontTypeCss, styledPageCss } from '../../../../styles/index.styled'
+import { FontType } from '../../../../styles/Theme'
 
 export const StyledUploadListingPage = styled.div`
   ${styledPageCss}
+  flex-direction: column;
+`
+
+export const UploadListingDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const TitleDiv = styled.div<{ fontType: FontType }>`
+  ${fontTypeCss}
+  width: 100%;
+  margin: 25px 0;
+`
+
+export const TitleHighlight = styled.span<{ type: 'Rent' | 'Sell' }>`
+  color: ${(props) =>
+    props.type === 'Rent' ? props.theme.palette.secondary : props.theme.palette.primary};
+`
+
+export const LeftDiv = styled.div`
+  ${borderedGreyDivCss}
+
+  width: 24vw;
+  height: 485px;
+`
+
+export const ItemPicture = styled.img`
+  width: 262px;
+  height: 262px;
+`
+
+export const PictureButton = styled(Button)`
+  height: 35px;
+  width: 100%;
+  border-radius: 35px;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme.palette.primary};
+  font-weight: bold;
+  font-size: 18px;
+
+  span {
+    color: ${(props) => props.theme.palette.common.white};
+  }
+
+  :hover {
+    background: ${(props) => props.theme.palette.highlight.dark};
+  }
+`
+
+export const RightDiv = styled.div`
+  ${borderedGreyDivCss}
+
+  width: 66vw;
+  height: 485px;
+  margin-left: 2.5vw;
+`
+
+export const PostForm = styled.form``
+
+export const EntryDiv = styled.div<{ type: 'input' | 'textarea' }>`
+  display: grid;
+  grid-template-columns: 2.5fr 7.5fr;
+  grid-gap: 12px;
+
+  width: 100%;
+  height: ${(props) => (props.type === 'input' ? '26px' : '106px')};
+  margin-bottom: 11px;
+`
+
+export const EntryArea = styled.textarea`
+  height: 106px;
+  border-radius: 16px;
+  padding: 8px 0 0 12px;
+`
+
+export const EntryName = styled.span<{ fontType: FontType }>`
+  ${fontTypeCss}
+  text-align: right;
+`
+
+export const TypeSelection = styled.select`
+  height: 26px;
+  width: 112px;
+`
+
+export const PostButton = styled(Button)`
+  height: 35px;
+  width: 100%;
+  border-radius: 35px;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme.palette.primary};
+  font-weight: bold;
+  font-size: 18px;
+
+  span {
+    color: ${(props) => props.theme.palette.common.white};
+  }
+
+  :hover {
+    background: ${(props) => props.theme.palette.highlight.dark};
+  }
 `
