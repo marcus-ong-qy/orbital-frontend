800a01ab4ee4cbabce0828cc74c2a6376e1f04aa
Marcus Ong <marcus.ong.qy@gmail.com>
2022-06-15 23:45:10 +0800

    added api data to user profile page

--- src/pages/Settings/UserProfilePage/index.tsx
+++ src/pages/Settings/UserProfilePage/index.tsx
@@ -1,36 +1,58 @@
 import { useState, useEffect } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
 
+// import { theme } from '../../../styles/Theme'
 import { auth, getUserFirebaseProfile } from '../../../firebase'
-import { defaultUserFirebaseProfile } from '../../../store/authentication/reducer'
-import { FirebaseProfile } from '../../../store/authentication/types'
+import { useAppDispatch, useAppSelector } from '../../../app/hooks'
+
+import { getUserData } from '../../../store/authentication/actions'
+import { defaultUserData, defaultUserFirebaseProfile } from '../../../store/authentication/reducer'
+import { FirebaseProfile, UserData } from '../../../store/authentication/types'
+
 import SettingsPageWrapper from '../SettingsPageWrapper'
 
 const UserProfilePage = () => {
+  const dispatch = useAppDispatch()
+  const { userData } = useAppSelector((state) => state.auth_reducer)
+
+  // const { navTitleFont, p } = { ...theme.typography.fontSize }
+
   const [userFirebaseProfile, setUserFirebaseProfile] = useState<FirebaseProfile>(
     defaultUserFirebaseProfile,
   )
 
-  // TODO show loading page
+  const [userDataHook, setUserDataHook] = useState<UserData>(defaultUserData)
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    userData && setUserDataHook(userData)
+  }, [userData])
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      if (user && userFirebaseProfile === defaultUserFirebaseProfile)
+      if (user && !isLoggedIn) {
         setUserFirebaseProfile(getUserFirebaseProfile(user))
+        setIsLoggedIn(true)
+        dispatch(getUserData(user))
+      } else if (!user && isLoggedIn) {
+        setUserFirebaseProfile(defaultUserFirebaseProfile)
+        setIsLoggedIn(false)
+      }
     })
   })
 
   return (
     <SettingsPageWrapper>
-      <h1>User Profile Page</h1>
-      <div>Name: </div>
-      <div>
-        Email: {userFirebaseProfile.email}&nbsp;
-        {userFirebaseProfile.emailVerified && '(Verified)'}
-      </div>
-      <div>
-        <div>Photo:</div>
-        <img src={''} alt="don't have" />
-      </div>
+      <h1>My Profile</h1>
+      <div>Name: {userDataHook.name}</div>
+      <div>Username: {userDataHook.username}</div>
+      <div>Phone: {userDataHook.phone}</div>
+      <div>Postal: {userDataHook.postal}</div>
+      <div>Email: {userFirebaseProfile.email}</div>
+      <div>Address: {userDataHook.address}</div>
+      <div>Gender: {userDataHook.gender}</div>
+      <div>DOB: {userDataHook.dob}</div>
     </SettingsPageWrapper>
   )
 }
