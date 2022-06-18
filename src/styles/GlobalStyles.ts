791a103d23958114cc14ae767d3a09e471d50660
Marcus Ong <marcus.ong.qy@gmail.com>
2022-06-18 15:55:44 +0800

    ensure all text adhere to global font

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -3,16 +3,16 @@ import { ThemeType } from './Theme'
 
 export const GlobalStyle = createGlobalStyle<{ theme: ThemeType }>`
   * {
-      box-sizing: border-box;
-      color: ${(props) => props.theme.palette.common.black};
+    box-sizing: border-box;
+    color: ${(props) => props.theme.palette.common.black};
+    font-family: ${(props) => props.theme.typography.main}, 
+      'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+      'Droid Sans', 'Helvetica Neue', sans-serif;
   }
 
   body {
     margin: 0;
     background: ${(props) => props.theme.palette.background};
-    font-family: ${(props) => props.theme.typography.main}, 
-        'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
-      'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
