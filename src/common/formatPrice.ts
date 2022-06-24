7af6b77e0828dd24bda40928bb2817662abdf830
Marcus Ong <marcus.ong.qy@gmail.com>
2022-06-25 00:48:55 +0800

    add func to format price

---
+++ src/common/formatPrice.ts
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param price number
+ * @returns price string formatted to 2 decimal places
+ */
+
+const formatPrice = (price: number) => price?.toFixed(2) ?? '-'
+
+export default formatPrice
